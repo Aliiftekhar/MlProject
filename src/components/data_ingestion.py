25617ba7fbff1e4d20efef51c6ada7c857d428a0
Aliiftekhar <iftekhar.ansari10@gmail.com>
2023-06-13 00:19:27 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifact',"train.csv")
@@ -52,4 +55,10 @@ if __name__=="__main__":
     train_data,test_data,=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_datatransformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_datatransformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill 
 
+from sklearn.metrics import r2_score 
 
 from src.exception import CustomException
 
@@ -16,5 +17,27 @@ def save_object(file_path, obj):
         with open(file_path,"wb") as file_obj:
             dill.dump(obj,file_obj)
 
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_model(x_train,y_train,x_test,y_test,models):
+    try:
+        report={}
+
+        for i in range(len(list(models))):
+            model= list(models.values())[i]
+
+            model.fit(x_train,y_train) #Train model
+
+            y_train_pred=model.predict(x_train)
+            y_test_pred=model.predict(x_test)
+
+            train_model_score=r2_score(y_train,y_train_pred)
+            test_model_score=r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[1]]=test_model_score
+        
+        return report
+    
     except Exception as e:
         raise CustomException(e,sys)
