3df97c01c0ed3d590285feed5d63bbeb659e3f32
Aliiftekhar <iftekhar.ansari10@gmail.com>
2023-06-13 16:45:33 +0530

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill 
 
 from sklearn.metrics import r2_score 
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -20,13 +21,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
 
         for i in range(len(list(models))):
             model= list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train) #Train model
 
             y_train_pred=model.predict(x_train)
